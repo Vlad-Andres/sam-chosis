d8f9371d3c6f76224741b0f985f5d37f91ab62f0
Vlad-Andres <Vlad-Andres@users.noreply.github.com>
2026-06-13 00:15:21 +0000

    deploy: f7ae15b14810d305cc0f581990f50962d4aa8566

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'schizo';
+var cacheName = 'sam-chosis';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
